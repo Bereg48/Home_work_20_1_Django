188864326ec69e22089488b937560d6f6baf7cf6
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-07-18 18:08:38 +0300

    version_to_check

---
+++ main/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from main.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
